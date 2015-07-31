7a078b6dc01e584474efa1a91ae6b4f10be9d334
sortofbusy <sortofbusy@gmail.com>
2015-07-31 13:58:01 -0700

    env config

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/eatthebible',
-	port: process.env.PORT || 8080,
+	port: process.env.PORT || 80,
 	assets: {
 		lib: {
 			css: [
