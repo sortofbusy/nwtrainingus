376b7ae7da9f612dfcb89682debfebd7a6c6e1e9
sortofbusy <sortofbusy@gmail.com>
2016-01-06 20:51:12 -0800

    home page formatting

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/eatthebible',
+	db: 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/eatthebible' || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI,
 	port: process.env.PORT || 80,
 	assets: {
 		lib: {
