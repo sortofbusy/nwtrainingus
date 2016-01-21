e69ac61504624bfa9c07ec4be78f209aaec8410a
sortofbusy <sortofbusy@gmail.com>
2016-01-20 19:34:21 -0800

    MongoLab 1st on production

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/nwtraining' || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI,
+	db: process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/nwtraining' || process.env.MONGOHQ_URL,
 	port: process.env.PORT || 80,
 	assets: {
 		lib: {
