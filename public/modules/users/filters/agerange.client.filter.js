3a099f0d30ca6da490a3cd3c5d5376f76f5ca808
sortofbusy <sortofbusy@gmail.com>
2016-03-01 23:58:38 -0800

    charts

--- public/modules/users/filters/agerange.client.filter.js
+++ public/modules/users/filters/agerange.client.filter.js
@@ -4,6 +4,7 @@ angular.module('users').filter('agerange', [
 	function() {
 		return function(applications, greaterThan, lowerThan) { 
 	        applications = applications.filter(function(application){
+	            if (!application.applicant || !application.applicant.age) return false;
 	            return application.applicant.age >= greaterThan && application.applicant.age <= lowerThan;
 	        });
 			return applications;
